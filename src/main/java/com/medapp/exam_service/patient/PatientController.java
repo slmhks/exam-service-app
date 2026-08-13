1981b036166e7dabd8aacfbfa9bc9aa2abeec27b
Roger SH <roger@test.com>
2026-08-13 18:50:01 -0300

    Add patient REST controller for handling HTTP requests

---
+++ src/main/java/com/medapp/exam_service/patient/PatientController.java
@@ -0,0 +1,28 @@
+package com.medapp.exam_service.patient;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDto> createPatient(@Valid @RequestBody CreatePatientRequestDto request) {
+        PatientResponseDto response = this.patientService.createPatient(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
